cf39e8b47847b15e0b2f2fc062c7f22424054587
Ilolo Izu <iloloizu97@gmail.com>
2021-12-13 15:33:51 -0600

    commit tree 5eecefb8079ee4b0d52bfd835b3782466cf93832

--- 01_js_variables_and_data_types/index.js
+++ 01_js_variables_and_data_types/index.js
@@ -2,13 +2,13 @@
 
 // ✅ Using console.log()
 
-    // console.log("Hey there!");
+    console.log("Hey there!");
 
-    // let playerStatus = "stopped";
+    let playerStatus = "stopped";
 
-    // let currentSong = null;
+    let currentSong = null;
 
-    // console.log("player status is", playerStatus);
+    console.log("player status is", playerStatus);
 
 // ✅ JS Data Types (Primitive)
 
@@ -41,16 +41,16 @@
     // Symbol => unique identifier (rarely used)
     // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Symbol
 
-        // let id = Symbol("test");
+        let id = Symbol("test");
 
-        // console.log(typeof id);
+        console.log(typeof id);
     
     // BigInt => numbers larger than those JS can represent with a Number
     // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/BigInt
 
-        // let bigNum = 123456789101112131415n;
+        let bigNum = 123456789101112131415n;
 
-        // console.log(typeof bigNum);
+        console.log(typeof bigNum);
 
 // ✅ let vs. const
 
@@ -61,7 +61,7 @@
 
         // let currentSong = "Sweet Dreams";
 
-        // let currentSong = "Cry Me a River";
+        // currentSong = "Cry Me a River";
         // console.log(currentSong);
 
     // const
@@ -69,7 +69,7 @@
 
         // const duration = 216;
 
-        // const duration;
+        // // const duration;
         
         // duration = 5;
 
@@ -77,43 +77,43 @@
 
     // if...else
 
-        // const duration = 216;
+        const duration = 216;
 
-        // if (duration < 180) {
-        //     console.log("This song is short!");
-        // } else {
-        //     console.log("This song isn't short.");
-        // }
+        if (duration < 180) {
+            console.log("This song is short!");
+        } else {
+            console.log("This song isn't short.");
+        }
 
     // if...else if...else
 
         // const duration = 216;
 
-        // if (duration > 240) {
-        //     console.log("This song is long!");
-        // } else if (duration < 180) {
-        //     console.log("This song is short!");
-        // } else {
-        //     console.log("This song is a normal length.");
-        // }
+        if (duration > 240) {
+            console.log("This song is long!");
+        } else if (duration < 180) {
+            console.log("This song is short!");
+        } else {
+            console.log("This song is a normal length.");
+        }
         
         
     // (Date Comparisons)
     
-        // let sweetDreamsLastPlayedAt = new Date("2021-12-12 10:30 AM")
-        // let cryMeARiverLastPlayedAt = new Date("2021-12-12 11:30 AM")
-        // console.log(sweetDreamsLastPlayedAt <= cryMeARiverLastPlayedAt)
+        let sweetDreamsLastPlayedAt = new Date("2021-12-12 10:30 AM")
+        let cryMeARiverLastPlayedAt = new Date("2021-12-12 11:30 AM")
+        console.log(sweetDreamsLastPlayedAt <= cryMeARiverLastPlayedAt)
 
     // ternary operator
     // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Conditional_Operator
 
-        // let favorite = true;
+        let favorite = true;
 
         // { favorite ? console.log("This song is my favorite!") : console.log("This song is not my favorite!") }
 
         // // or 
 
-        // console.log(`This song ${favorite ? 'is' : 'is not'} my favorite!`)
+        console.log(`This song ${favorite ? 'is' : 'is not'} my favorite!`)
 
 console.log("------------------------");
 console.log("⬇️ Break Out Activities ⬇️");
@@ -136,16 +136,17 @@ console.log("------------------------");
     // this line of code generates a random number between 0 and 24 and assigns its value to `time`
     const time = Math.ceil(Math.random()*24);
     // // 🚧 🚧 🚧 🚧 🚧 🚧 🚧 🚧 🚧 🚧 🚧 🚧 🚧 🚧
-    // if (❓) {
 
-        // console.log(`${time} is in the AM.`)
+    if (time < 12) { 
+
+        console.log(`${time} is in the AM.`)
 
     // In all other cases...
-    // } else {
+    } else {  
 
-        // console.log(`${time} is in the PM.`)
+        console.log(`${time} is in the PM.`)
 
-    // }
+    }
     // // 🚧 🚧 🚧 🚧 🚧 🚧 🚧 🚧 🚧 🚧 🚧 🚧 🚧 🚧
 
 
